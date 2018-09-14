10131088c91cfca928db720b68c3e0a8d3e3b489
jdevmx <33272633+jdevmx@users.noreply.github.com>
2018-09-14 15:24:51 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+# ######################### #
+#                           #
+# Autor: Jorge Dominguez    #
+#                           #
+# MandarinaSoft - 2018      #
+#                           #
+# ######################### #
+
+from kivy.app import App
+
+from login.login import Login
+
+
+class MainApp(App):
+
+    def build(self):
+        pass
+
+
+app = MainApp()
+app.run()
